8430ea8966e7ba7c945f3a5503884b9555d57ca9
akshit913 <akshit.9@gmail.com>
2020-05-11 20:17:39 -0400

    css not load

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import './public/css/autoptimize_b98fe647c7bf321ab61734b41f007e11.css'
-import './public/css/autoptimize_single_8d20a0fee59b2392d59a6b46fe11432c.css'
-import './public/css/autoptimize_single_942530f201a7fd5e7649dfff13944c9a.css'
-import './public/css/main.css'
-import './public/css/style.css'
+// import './index.css';
+// import './public/css/autoptimize_b98fe647c7bf321ab61734b41f007e11.css'
+// import './public/css/autoptimize_single_8d20a0fee59b2392d59a6b46fe11432c.css'
+// import './public/css/autoptimize_b98fe647c7bf321ab61734b41f007e11.css'
 ReactDOM.render(
   <App />,
   document.getElementById('root')
